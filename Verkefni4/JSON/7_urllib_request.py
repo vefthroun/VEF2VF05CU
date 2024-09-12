bb042135df0173c7f327fe2152818f9789a29310
Gunnar Þórunnarson <gus@tskoli.is>
2024-09-12 21:22:39 +0000

    Update 7_urllib_request.py

--- Verkefni4/JSON/7_urllib_request.py
+++ Verkefni4/JSON/7_urllib_request.py
@@ -1,12 +1,7 @@
 # urllib til að ná json skrár yfir netið
 import urllib.request, json
 
-# Fixing the SSL CERTIFICATE_VERIFY_FAILED Error
-# https://realpython.com/urllib-request/#fixing-the-ssl-certificate_verify_failed-error  
-# Lausn fyrir Mac notendur: Applications Folder -> Python folder, and click on the Install Certificates.command file
-
-# JSON skrá hýst á Github sem Gist (max 60 request pr klst nema notað sé token). Vistaðu skránna með .JSON sniðmáti og veldu `raw` og afritaðu vefslóðina (url).
-# Dæmi
+# JSON skrá hýst á Github (https) sem Gist (max 60 request pr klst nema notað sé token). Vistaðu skránna með .JSON sniðmáti og veldu `raw` og afritaðu vefslóðina (url).
 with urllib.request.urlopen("https://gist.githubusercontent.com/GunnarThorunnarson/13839d272efae357adc1edbcd91f2938/raw/0175db90b92972daf7cf5142a986f28c9e923ae4/nemendur.json") as response:
     gogn = json.loads(response.read())
 print(gogn)
