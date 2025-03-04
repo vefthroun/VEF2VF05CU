788f6788372ef7095d578aa5d3e32d84b5f08765
gjg <gjg@tskoli.is>
2025-03-04 17:43:03 +0000

    url 4

--- Verkefni1/namsefni/urlfor.py
+++ Verkefni1/namsefni/urlfor.py
@@ -9,26 +9,27 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return "<a href="{{ url_for('index') }}">Link To Index</a>"    # dæmi um html hlekk: 
+@app.route('/heimaerbest')
+def home():
+    return "<a href=" + url_for('sida2') + ">Link To Gunnar</a>"    # dæmi um html hlekk: 
 
-@app.route('/sida1')
+@app.route('/sida1/') # ath! /.../
 def sida1():
-    # vísar í routið sem geymir index fallið og skilar því urli
-    return url_for("index")  # /
+    # vísar í routið sem geymir home fallið og skilar því + seinna urlinu
+    return url_for("home")  # /
 
 @app.route('/sida2')
 def sida2():
-    # vísar í routið sem geymir sida3 fallið og skilar því urli
-    return url_for("sida3", name="Gunnar")  # /sida3/Gunnar
-
+    # vísar í routið sem geymir sida3 fallið og skilar því urli ásamt færibreytu
+    return url_for("sida3", name="Gunnar") 
+ 
+# /sida3 birtist ekki nema í gegnum /sida2
 @app.route('/sida3/<string:name>')
 def sida3(name):
-    return "The name is " + name  # The name is Gunnar
+    return  name # birtir /sida3/Gunnar
 
-
-# Vísun í static möppu sem inniheldur CSS skrá (auðveldara að nota með template):  
-# url_for('static', filename='style.css') 
+# Vísun í static möppu sem inniheldur CSS skrá
+# í jinja2 template: url_for('static', filename='style.css') 
 
 if __name__ == '__main__':
   app.run(debug=True, use_reloader=True)  
