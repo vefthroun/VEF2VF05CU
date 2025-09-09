2c115eb813c46ab855449aebee4b7a67d6ad2b8a
gjg <gjg@tskoli.is>
2025-09-09 13:18:44 +0000

    indent ok

--- Verkefni4/JSON/3_JsonToDictionary.py
+++ Verkefni4/JSON/3_JsonToDictionary.py
@@ -2,10 +2,10 @@ import json
 
     # To change string to JSON
     # Take the following string containing JSON data:
-    json_string = '{"first_name": "Guido", "last_name":"Rossum"}'
+json_string = '{"first_name": "Guido", "last_name":"Rossum"}'
 
     # It can be parsed like this:
-    parsed_json = json.loads(json_string)
+parsed_json = json.loads(json_string)
 
     # and can now be used as a normal dictionary:
-    print(parsed_json)
+print(parsed_json)
